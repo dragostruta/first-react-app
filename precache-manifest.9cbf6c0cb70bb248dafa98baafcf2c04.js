a98133ccf5ea94990f438fa264966659d3c5a895
Truta Dragos <truta_dragos1997@icloud.com>
2019-10-07 03:09:47 +0300

    Updates

---
+++ precache-manifest.9cbf6c0cb70bb248dafa98baafcf2c04.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9cf4724bb55950bc813faaf454210cd6",
+    "url": "/first-react-app/index.html"
+  },
+  {
+    "revision": "f5fb766a55a76e50c9d8",
+    "url": "/first-react-app/static/css/2.0624914a.chunk.css"
+  },
+  {
+    "revision": "6e68ff166d5f8cfd5e56",
+    "url": "/first-react-app/static/css/main.34de6062.chunk.css"
+  },
+  {
+    "revision": "f5fb766a55a76e50c9d8",
+    "url": "/first-react-app/static/js/2.ccbca48e.chunk.js"
+  },
+  {
+    "revision": "6e68ff166d5f8cfd5e56",
+    "url": "/first-react-app/static/js/main.b41367cf.chunk.js"
+  },
+  {
+    "revision": "f1c006e2d2a0ca2827e0",
+    "url": "/first-react-app/static/js/runtime-main.7fe0590d.js"
+  }
+]);
